0d51a353a1330d6673632ac3b38eeb3a673bfaaf
and12241 <kolesnikv816@gmail.com>
2024-09-14 13:22:47 +0300

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 const refs = {
-   inputWeather: document.querySelector(".input-location"),
+inputWeather: document.querySelector(".input-location"),
    btnSearch: document.querySelector(".search-btn"),  
    body: document.querySelector("body"),
    tempMain: document.querySelector('.temp-info'),
@@ -17,6 +17,45 @@ refs.btnSearch.addEventListener('click', () => {
     const location = refs.inputWeather.value;
     console.log(location);
     fetch(`http://api.weatherstack.com/current?access_key=c17247bb0cec3cd6c40a1334acc604c5&query=${location}`).then(response=>response.json()).then(weatherShow => {
+        weatherChange(weatherShow);
+
+        switch(weatherShow.current.weather_descriptions[0]) {
+            case "Heavy rain":
+            case "Heavy rain at times": {
+                refs.weatherIcon.src = "./images/storm-icon.png";
+                refs.body.style.backgroundImage = 'url("/images/weather-bg/bg-storm.jpg")';
+                break;
+            }
+            case "Sunny":
+            case "Clear": {
+                refs.weatherIcon.src = "./images/sunny-icon.png";
+                refs.body.style.backgroundImage = 'url("./images/weather-bg/sunny-bg.jpg")';
+                break;
+            }
+            case 'Partly Cloudy':
+            case "Cloudy":
+            case "Overcast": {
+                refs.weatherIcon.src = "./images/Cloudy.svg";
+                refs.body.style.backgroundImage = 'url("./images/weather-bg/bg-cloud.jpg")';
+                break;
+            }
+            case "Patchy rain nearby":
+            case 'Light Rain, Rain':
+            case "Patchy light rain":
+            case "Light rain":
+            case "Moderate rain at times":
+            case "Moderate rain":
+            case "Light rain,rain": {
+                refs.weatherIcon.src = "./images/Cloudy.svg";
+                refs.body.style.backgroundImage = 'url("./images/weather-bg/bg-rain.jpg")';
+                break;
+            }
+        }
+        
+    }).catch();
+})
+
+function weatherChange (weatherShow) {
         refs.tempMain.textContent = `${weatherShow.current.temperature}°`; 
         refs.cityText.textContent = `${weatherShow.location.name}`;
         refs.typeWeather.textContent = `${weatherShow.current.weather_descriptions[0]}`;
@@ -24,10 +63,8 @@ refs.btnSearch.addEventListener('click', () => {
         refs.humadityText.textContent = `${weatherShow.current.humidity}%`;
         refs.cloudyText.textContent = `${weatherShow.current.cloudcover}%`;
         refs.windText.textContent = `${weatherShow.current.wind_speed}km/h`;
-        refs.weatherIcon.src = weatherShow.current.weather_icons[0];
+        //refs.weatherIcon.src = weatherShow.current.weather_icons[0];
         console.log(weatherShow);
         
         refs.mainInfo.classList.add('active');
-    }).catch()
-})
-
+}
